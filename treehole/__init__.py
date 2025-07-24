afdc419a38809f4e8e2d769885a314b2511e34a7
mywaiting <ourwaiting@gmail.com>
2025-07-24 13:15:04 +0800

    完整整个项目打包的 pyproject.toml 的配置，注意需要提前编译好 po -> mo 国际化文件

---
+++ treehole/__init__.py
@@ -0,0 +1,21 @@
+# 
+# TreeHole
+# 
+
+"""Microblog platform based Github issues."""
+
+from .__version__ import (
+    version,
+    version_tuple,
+    __version__,
+    __version_tuple__
+)
+
+
+__title__ = "treehole"
+__description__ = "Microblog platform based Github issues."
+__url__ = "https://treehole.io"
+__author__ = "mywaiting"
+__email__ = "hi@mywaiting.com"
+__copyright__ = "Copyright Mywaiting"
+
